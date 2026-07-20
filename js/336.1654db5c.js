f8fe964877058ebd60b02f7d1961212a8a116795
pferreirafabricio <pferreirafabricio@gmail.com>
2026-07-20 19:41:54 +0000

    Deploying to gh-pages from @ pferreirafabricio/popover-search-ionic@afacd336a40efd49b8d2265962d67e91293c0a16 🚀

---
+++ js/336.1654db5c.js
@@ -0,0 +1,6 @@
+"use strict";(globalThis["webpackChunkpopover_search_ionic"]=globalThis["webpackChunkpopover_search_ionic"]||[]).push([[336],{8336(e,t,o){var n=o(8993),s=o(4534),r=o(9185);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const i=()=>{const e=window;e.addEventListener("statusTap",()=>{(0,n.gv)(()=>{const t=e.innerWidth,o=e.innerHeight,i=document.elementFromPoint(t/2,o/2);if(!i)return;const a=(0,s.a)(i);a&&new Promise(e=>(0,r.c)(a,e)).then(()=>{(0,n.bN)(async()=>{a.style.setProperty("--overflow","hidden"),await(0,s.s)(a,300),a.style.removeProperty("--overflow")})})})})};o.d(t,["startStatusTap",0,i])}}]);
+//# sourceMappingURL=336.1654db5c.js.map
